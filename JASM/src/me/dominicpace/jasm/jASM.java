7da0164a39c1bf0b4e7b5669dd0a1263edcad94c
Dominic Pace <dompace181@gmail.com>
2017-11-14 08:40:48 -0600

    8 bit register class

--- JASM/src/me/dominicpace/jasm/jASM.java
+++ JASM/src/me/dominicpace/jasm/jASM.java
@@ -3,7 +3,7 @@ package me.dominicpace.jasm;
 public class jASM {
 
     public static void main(String[] args) {
-        System.out.println("Vopi's Java 8080 assembler");
+        System.out.println("Vopi's Java 8080 assembler!");
         if(args.length < 2) {
             System.out.println("USAGE: java jASM <input.s>");
         }
