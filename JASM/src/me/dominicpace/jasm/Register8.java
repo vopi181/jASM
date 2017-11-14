6ab3b88c28910b310e5f86dc6085a618e28970d1
Dominic Pace <dompace181@gmail.com>
2017-11-14 08:40:27 -0600

    8 bit register class

---
+++ JASM/src/me/dominicpace/jasm/Register8.java
@@ -0,0 +1,22 @@
+package me.dominicpace.jasm;
+
+
+/**
+ * This is the Register class for 8 bit 8080 instructions
+ * @author vopi
+ *
+ */
+public class Register8 {
+    /**
+     * Mnemonic like MOV, ADD, etc
+     */
+    public String mnemonic;
+
+    /**
+     * Hex encoding of the instruction like 0x00, etc
+     */
+    public int hex_encoding;
+
+
+
+}
